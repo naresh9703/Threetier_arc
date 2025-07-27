3bf956fa398edd9c3e968001e897bbf6e768023c
naresh <nareshveeranala@gmail.com>
2025-07-27 17:07:26 +0530

    creating instaces

--- backend.sh
+++ backend.sh
@@ -6,89 +6,83 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD #absolute path
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
-
-# check the user has root priveleges or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
-
-# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-    if [ $1 -eq 0 ]
+    if [ $1 -ne 0 ]
     then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
+}
 
+mkdir -p $LOGS_FOLDER
 
-dnf module disable nodejs -y &>>$LOG_FILE 
-VALIDATE $? "Disabling default nodejs"
+echo "Script started executing at: $TIMESTAMP"
 
-dnf module enable nodejs:20 -y &>>$LOG_FILE 
-VALIDATE $? "Enabling nodejs:20"
+CHECK_ROOT
 
-dnf install nodejs -y &>>$LOG_FILE 
-VALIDATE $? "Installing nodejs:20"
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling existing default NodeJS"
 
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20"
 
-id expense
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
+
+id expense &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    useradd --system --home /app --shell /sbin/nologin --comment "expense user" expense &>>$LOG_FILE
-    VALIDATE $? "Creating expense user"
+    useradd expense &>>$LOG_FILE
+    VALIDATE $? "Adding expense user"
 else
-    echo -e "System user expense already created ... $Y SKIPPING $N"
+    echo -e "expense user already exists ... $Y SKIPPING $N"
 fi
 
 mkdir -p /app &>>$LOG_FILE
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
-VALIDATE $? "Downloading Catalogue"
+VALIDATE $? "Downloading backend"
 
+cd /app
 rm -rf /app/*
-cd /app 
+
 unzip /tmp/backend.zip &>>$LOG_FILE
-VALIDATE $? "unzipping backend"
+VALIDATE $? "unzip backend"
 
 npm install &>>$LOG_FILE
-VALIDATE $? "Installing Dependencies"
+VALIDATE $? "Installing dependencies"
 
-#we can give absolute path in that where ever the script it will execute
+cp /home/ec2-user/shell-project/backend.service /etc/systemd/system/backend.service
 
-cp $SCRIPT_DIR/backend.service /etc/systemd/system/backend.service 
-VALIDATE $? "Copying backend service"
-
+# Prepare MySQL Schema
 
 dnf install mysql -y &>>$LOG_FILE
-VALIDATE $? "Installing mysql"
+VALIDATE $? "Installing MySQL Client"
 
-#read -s MYSQL_ROOT_PASSWORD
-
-mysql -h mysql.nareshveeranala.shop -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+mysql -h mysql.jyothi.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
 VALIDATE $? "Setting up the transactions schema and tables"
 
-
 systemctl daemon-reload &>>$LOG_FILE
-systemctl start backend &>>$LOG_FILE
+VALIDATE $? "Daemon Reload"
+
 systemctl enable backend &>>$LOG_FILE
-VALIDATE $? "bakend"
+VALIDATE $? "Enabling backend"
 
 systemctl restart backend &>>$LOG_FILE
-VALIDATE $? "restarting backend"
+VALIDATE $? "Starting Backend"
