b63a849ed3570cc3a7e61d007deb549db14cdb89
naresh <nareshveeranala@gmail.com>
2025-07-27 17:27:12 +0530

    creating instaces

--- frontend.sh
+++ frontend.sh
@@ -63,6 +63,6 @@ systemctl restart nginx
 VALIDATE $? "Restarting nginx"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME-$START_TIME ))
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
